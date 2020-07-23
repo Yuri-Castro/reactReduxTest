79a10bad8ca64bbed653e3f7dab10ff07be8b941
Yuri <yuricda96@gmail.com>
2020-07-23 01:55:56 -0300

    feat(simple example with redux and reac-redux done) git push origin master

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import SideBar from './components/SideBar';
+import Video from './components/Video';
+import {Provider} from 'react-redux';
+import store from './store';
+
+function App() {
+  return (
+    <div className="App">
+      <Provider store={store}>
+        <Video/>
+        <SideBar/>        
+      </Provider>
+    </div>
+  );
+}
+
+export default App;
